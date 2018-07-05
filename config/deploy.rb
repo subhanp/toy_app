206c10a5dfabaf7adbb591adf0e50fc525bd14ba
subhankhanp <subhankhan.p@bitlasoft.com>
2018-07-05 17:29:55 +0530

    deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,11 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_bind,       "unix://#{shared_path}/shared/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/shared/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/shared/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/shared/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/shared/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -31,8 +31,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ set :linked_files, %w{config/database.yml config/secrets.yml}
+ set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
